b23ed54fe70e143a2983fee7840bdcfbf9b699e4
liangdaji <liangdaji@huobi.com>
2021-05-24 17:00:19 +0800

    AddressList: fix blacklist returns

--- contracts/AdressList.sol
+++ contracts/AdressList.sol
@@ -11,10 +11,6 @@ contract AddressList {
         To,
         Both
     }
-    struct Blacklist {
-        address a;
-        Direction d;
-    }
 
     bool public initialized;
     bool public devVerifyEnabled;
@@ -23,7 +19,9 @@ contract AddressList {
 
     mapping(address => bool) private devs;
 
-    Blacklist[] blacks;
+    //NOTE: make sure this list is not too large!
+    address[] blacksFrom;
+    address[] blacksTo;
 
     event EnableStateChanged(bool indexed newState);
 
@@ -87,29 +85,69 @@ contract AddressList {
         return devs[addr];
     }
 
-    function getBlacklist() view external returns (Blacklist[] memory) {
-        return blacks;
+    function getBlacksFrom() view external returns (address[] memory) {
+        return blacksFrom;
+    }
+
+    function getBlacksTo() view external returns (address[] memory) {
+        return blacksTo;
     }
 
     function addBlacklist(address a, Direction d) external onlyAdmin {
-        // add with NO check
-        Blacklist memory b = new Blacklist(a,d);
-        blacks.push(b);
+        if (d == Direction.Both) {
+            blacksFrom.push(a);
+            blacksTo.push(a);
+        } else if (d == Direction.From) {
+            blacksFrom.push(a);
+        } else {
+            blacksTo.push(a);
+        }
 
         emit BlackAddrAdded(a,d);
     }
 
+    function removeBlacklistAtIndex(uint i, Direction d) external onlyAdmin {
+        require(d != Direction.Both, "can not be Both on removeBlacklistAtIndex");
+        if (d == Direction.From) {
+            removeBlacksAtIndex(blacksFrom,i,d);
+        } else {
+            removeBlacksAtIndex(blacksTo,i,d);
+        }
+    }
+
     function removeBlacklist(address a, Direction d) external onlyAdmin {
+        if (d == Direction.Both || d == Direction.From) {
+            removeBlacks(blacksFrom, a, Direction.From);
+            removeBlacks(blacksTo, a, Direction.To);
+        } else if (d == Direction.From) {
+            removeBlacks(blacksFrom, a, d);
+        } else {
+            removeBlacks(blacksTo, a, d);
+        }
+    }
+
+    function removeBlacks(address[] storage blacks, address a, Direction d) private {
         for (uint i = 0; i < blacks.length; i++) {
-            if (blacks[i].a == a && (d == Direction.Both || blacks[i].d == d)) {
-                Direction originD = blacks[i].d;
+            if (blacks[i] == a ) {
                 if (i != blacks.length - 1) {
                     blacks[i] = blacks[blacks.length - 1];
                 }
                 blacks.pop();
 
-                emit BlackAddrRemoved(a,originD);
+                emit BlackAddrRemoved(a,d);
+                break;
             }
         }
+    }
+
+    function removeBlacksAtIndex(address[] storage blacks, uint i, Direction d) private {
+        require(i < blacks.length,"index out of bound");
+        address a = blacks[i];
+        if (i != blacks.length - 1) {
+            blacks[i] = blacks[blacks.length - 1];
+        }
+        blacks.pop();
+
+        emit BlackAddrRemoved(a,d);
     }
 }
